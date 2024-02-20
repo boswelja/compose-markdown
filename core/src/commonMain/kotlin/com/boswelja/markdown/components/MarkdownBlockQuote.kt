b09c6d02a470d6b06ba2628c188bc12b4958c67a
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-02-20 20:20:19 +1300

    Add code block indicator

--- core/src/commonMain/kotlin/com/boswelja/markdown/components/MarkdownBlockQuote.kt
+++ core/src/commonMain/kotlin/com/boswelja/markdown/components/MarkdownBlockQuote.kt
@@ -4,9 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import com.boswelja.markdown.MarkdownNode
 import com.boswelja.markdown.generator.MarkdownBlockQuote
 import com.boswelja.markdown.style.BlockQuoteStyle
@@ -14,7 +20,6 @@ import com.boswelja.markdown.style.CodeBlockStyle
 import com.boswelja.markdown.style.RuleStyle
 import com.boswelja.markdown.style.TextStyleModifiers
 import com.boswelja.markdown.style.TextStyles
-import com.boswelja.markdown.toDp
 
 /**
  * Displays a [MarkdownBlockQuote]. A block quote is a visually distinct section in a document,
@@ -32,22 +37,34 @@ internal fun MarkdownBlockQuote(
 ) {
     Box(
         modifier = Modifier
-            .background(style.background, style.shape)
+            .clip(style.shape)
+            .background(style.background)
             .then(modifier)
     ) {
-        Column(
-            modifier = Modifier.padding(style.innerPadding),
-            verticalArrangement = Arrangement.spacedBy(textStyles.textStyle.fontSize.toDp())
-        ) {
-            blockQuote.children.forEach {
-                MarkdownNode(
-                    node = it,
-                    textStyles = textStyles,
-                    textStyleModifiers = textStyleModifiers,
-                    blockQuoteStyle = style,
-                    codeBlockStyle = codeBlockStyle,
-                    ruleStyle = ruleStyle
+        Row(Modifier.height(IntrinsicSize.Min)) {
+            Box(
+                modifier = Modifier
+                    .clip(style.barShape)
+                    .background(style.barColor)
+                    .width(style.barWidth)
+                    .fillMaxHeight()
+            )
+            Column(
+                modifier = Modifier.padding(style.innerPadding),
+                verticalArrangement = Arrangement.spacedBy(
+                    style.innerPadding.calculateBottomPadding()
                 )
+            ) {
+                blockQuote.children.forEach {
+                    MarkdownNode(
+                        node = it,
+                        textStyles = textStyles,
+                        textStyleModifiers = textStyleModifiers,
+                        blockQuoteStyle = style,
+                        codeBlockStyle = codeBlockStyle,
+                        ruleStyle = ruleStyle,
+                    )
+                }
             }
         }
     }
