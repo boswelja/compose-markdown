9e0c4e5e6b756177a7d2bfe0188ff26514d81368
Jack Boswell <boswelja@outlook.com>
2024-02-21 16:52:33 +1300

    Handle stray braces (#6)

--- core/src/commonMain/kotlin/com/boswelja/markdown/generator/MarkdownNodeGenerator.kt
+++ core/src/commonMain/kotlin/com/boswelja/markdown/generator/MarkdownNodeGenerator.kt
@@ -135,6 +135,8 @@ internal class MarkdownNodeGenerator(
             MarkdownTokenTypes.HTML_TAG,
             MarkdownTokenTypes.HTML_BLOCK_CONTENT,
             MarkdownTokenTypes.COLON,
+            MarkdownTokenTypes.LBRACKET,
+            MarkdownTokenTypes.RBRACKET,
             MarkdownElementTypes.EMPH -> parseTextNode(astNode)
             MarkdownTokenTypes.WHITE_SPACE -> MarkdownWhitespace
             MarkdownTokenTypes.EOL -> MarkdownEol
@@ -223,6 +225,8 @@ internal class MarkdownNodeGenerator(
             MarkdownTokenTypes.HTML_TAG,
             MarkdownTokenTypes.HTML_BLOCK_CONTENT,
             MarkdownTokenTypes.COLON,
+            MarkdownTokenTypes.LBRACKET,
+            MarkdownTokenTypes.RBRACKET,
             MarkdownTokenTypes.TEXT -> MarkdownText(
                 text = astNode.getTextInNode(allFileText).trim().toString(),
                 isBold = false,
