2c9cddaef10d1af3515cf92cba9d16befcf801b9
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-11-13 20:02:01 +1100

    Update dependencies

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -20,12 +20,12 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
     implementation(project(":material3"))
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation(compose.material3)
 }
