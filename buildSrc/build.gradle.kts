d53920a8ffa8bc1af04f2374408cf790696d43b4
Jack Boswell <boswelja@outlook.com>
2025-05-25 11:50:26 +1000

    Migrate to third party publish plugin (#83)
    * Migrate to third party publish plugin
    
    * Update workflows
    
    * 1.1.4
    
    * Add missing code scanning permission

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     `kotlin-dsl`
 }
+
+dependencies {
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.32.0")
+    implementation(libs.android.libraryGradlePlugin)
+    implementation(libs.kotlin.multiplatformGradlePlugin)
+}
--- buildSrc/src/main/kotlin/com.boswelja.publish.gradle.kts
+++ buildSrc/src/main/kotlin/com.boswelja.publish.gradle.kts
@@ -1,6 +1,7 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish")
 }
 
 interface CustomPublishingExtension {
@@ -14,73 +15,43 @@ val extension = project.extensions.create<CustomPublishingExtension>("publish")
 group = findProperty("group")!!
 version = findProperty("version")!!
 
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
-}
-
 afterEvaluate {
     publishing {
         repositories {
-            if (System.getenv("PUBLISHING") == "true") {
-                maven("https://maven.pkg.github.com/boswelja/compose-markdown") {
-                    val githubUsername: String? by project.properties
-                    val githubToken: String? by project.properties
-                    name = "github"
-                    credentials {
-                        username = githubUsername
-                        password = githubToken
-                    }
-                }
-                maven("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/") {
-                    val ossrhUsername: String? by project
-                    val ossrhPassword: String? by project
-                    name = "oss"
-                    credentials {
-                        username = ossrhUsername
-                        password = ossrhPassword
-                    }
-                }
-            }
-        }
-
-        publications.withType<MavenPublication> {
-            val javadocJar = tasks.register("${name}JavadocJar", Jar::class) {
-                archiveClassifier.set("javadoc")
-                // Each archive name should be distinct. Mirror the format for the sources Jar tasks.
-                archiveBaseName.set("${archiveBaseName.get()}-${name}")
-            }
-            artifact(javadocJar)
-            pom {
-                name = project.name
-                description = extension.description
-                url = extension.repositoryUrl.get()
-                licenses {
-                    license {
-                        name = extension.license.get()
-                        url = "${extension.repositoryUrl}/blob/main/LICENSE"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "boswelja"
-                        name = "Jack Boswell (boswelja)"
-                        email = "boswelja@outlook.com"
-                        url = "https://github.com/boswelja"
-                    }
-                }
-                scm {
-                    url.set(extension.repositoryUrl.get())
-                }
+            maven("https://maven.pkg.github.com/boswelja/compose-markdown") {
+                name = "github"
+                credentials(PasswordCredentials::class)
             }
         }
     }
 
-    // TODO: Remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
-    //  Thanks to KSoup repository for this code snippet
-    tasks.withType(AbstractPublishToMaven::class).configureEach {
-        dependsOn(tasks.withType(Sign::class))
+    mavenPublishing {
+        coordinates(group as String, name, version as String)
+
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+        signAllPublications()
+
+        pom {
+            name = project.name
+            description = extension.description
+            url = extension.repositoryUrl.get()
+            licenses {
+                license {
+                    name = extension.license.get()
+                    url = "${extension.repositoryUrl}/blob/main/LICENSE"
+                }
+            }
+            developers {
+                developer {
+                    id = "boswelja"
+                    name = "Jack Boswell (boswelja)"
+                    email = "boswelja@outlook.com"
+                    url = "https://github.com/boswelja"
+                }
+            }
+            scm {
+                url.set(extension.repositoryUrl.get())
+            }
+        }
     }
 }
